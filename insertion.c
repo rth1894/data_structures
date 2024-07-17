f8a78071f0c78e58f75b95ffd5cc6eb6164ef808
rth1894 <tupeparth72@gmail.com>
2024-07-17 23:06:03 +0530

    added insertion sort

---
+++ insertion.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void print(int size, int array[]);
+void sort(int size, int array[]);
+void swap(int* x, int* y);
+
+void main() {
+        int n;
+        printf("Enter number of array elements: ");
+        scanf("%d", &n);
+ 
+        int a[n];
+        for (int i=0; i<n; i++) {
+                printf("Element %d of array: ", i+1);
+                scanf("%d", &a[i]);
+        }
+        printf("Initial: ");
+        print(n, a);
+        printf("Sorted: ");
+        sort(n, a);
+}
+
+void print(int size, int array[]) {
+        for(int i=0; i<size; i++) {
+                printf("%d ", array[i]);
+        }
+        printf("\n");
+}
+
+void sort(int size, int array[]) {
+	int key, j;
+	for (int i=0; i<size; i++) {
+		key = array[i];
+		j = i - 1;
+		while (j>=0 && array[j]>key) {
+			array[j+1] = array[j];
+			j--;
+		}
+		array[j+1] = key;
+	}
+	print(size, array);
+}
+
+void swap(int* x, int* y) {
+        int temp = *x;
+        *x = *y;
+        *y = temp;
+}
